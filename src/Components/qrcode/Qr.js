b82c5640cb092c9fb9c7b461a351660642f6343a
rohankurella02 <rohankurella02@gmail.com>
2023-05-01 12:32:18 +0530

    added authentication

--- src/Components/qrcode/Qr.js
+++ src/Components/qrcode/Qr.js
@@ -126,7 +126,7 @@ function Qr() {
                   <option value={"user"}>Front Camera</option>
               </select>
           </div>
-          {startScan && (
+          {true && (
               <>
                   
                   <div className={startScan ? "qr_reader" : "hidden"}>
